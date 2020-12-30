4ea7da9e9e0063b1432016ba4e6d4493d561f208
Daichi-Uematsu <cy17208@shibaura-it.ac.jp>
2020-12-30 19:25:39 +0900

    Aboutページ修正

--- shibatest/src/About.js
+++ shibatest/src/About.js
@@ -28,20 +28,22 @@ const containerVariants ={
     }
   }
 
+
+
 function About() {
 
   return (
+    
     <motion.div 
-    variants={containerVariants}
-    initial="hidden"
-    animate="visible"
-    exit="exit"
-    className="About">
-        
+        variants={containerVariants}
+        initial="hidden"
+        animate="visible"
+        exit="exit"
+        className="About">
+    
         <PageTop 
             title="ABOUT"
             text="展覧会について"
-            current="ABOUT"
         />
         <div className="container">
             
@@ -69,26 +71,30 @@ function About() {
 
             <div className="concept">
                 <h2>コンセプト</h2>
-                <ul>
-                    <li>
-                        <div className="conceptText">
-                        <p>ハカる</p>
+
+                <div className="conceptContents">
+
+                        <div className="conceptTitle">
+                            <p>ハカる</p>
                         </div>
-                    </li>
-                    <li>
-                        <p>なぜ「ハカる」にしたのか。その背景や理由などをここに記述す<br></br>る。卒展に込める思い、来場する人に何を伝えたいのか。<br></br>
+
+                        <div className="conceptText">
+                        <p>なぜ「ハカる」にしたのか。その背景や理由などをここに記述する。卒展に込める思い、来場する人に何を伝えたいのか。<br></br>
                         <br></br>
-                        なぜ「ハカる」にしたのか。その背景や理由などをここに記述す<br></br>る。卒展に込める思い、来場する人に何を伝えたいのか。<br></br>
-                        なぜ「ハカる」にしたのか。その背景や理由などをここに記述す<br></br>る。卒展に込める思い、来場する人に何を伝えたいのか。<br></br>
+                        なぜ「ハカる」にしたのか。その背景や理由などをここに記述する。卒展に込める思い、来場する人に何を伝えたいのか。<br></br>
+                        なぜ「ハカる」にしたのか。その背景や理由などをここに記述する。卒展に込める思い、来場する人に何を伝えたいのか。<br></br>
                         <br></br>
-                        なぜ「ハカる」にしたのか。その背景や理由などをここに記述す<br></br>る。卒展に込める思い、来場する人に何を伝えたいのか。</p>
-                </li>
-                </ul>
+                        なぜ「ハカる」にしたのか。その背景や理由などをここに記述する。卒展に込める思い、来場する人に何を伝えたいのか。</p>
+                        </div>
+                        
+                </div>
+
             </div>
 
-            <div className="about-contents">
+            <div className="overView">
              <h2>開催概要</h2>
-            <img src={aboutImage} className="about-image" alt="exhibitionPoster"/>
+
+            <div className="overViewcontents">
 
                 <div className="about-texts">
                     <div className="about-texts-row mg-16">
@@ -110,11 +116,11 @@ function About() {
                     </div>
 
                     <div className="about-texts-row mg-16">
-                    <h4>会場</h4>
-                    <div>
-                        <p>芝浦工業大学芝浦キャンパス<br></br>〒108-8548 東京都港区芝浦3-9-14</p>
-                        <p className="about-subtext">*大学敷地内に駐車場・駐輪場はございません。</p>
-                    </div>
+                        <h4>会場</h4>
+                        <div>
+                         <p>芝浦工業大学芝浦キャンパス<br></br>〒108-8548 東京都港区芝浦3-9-14</p>
+                         <p className="about-subtext">*大学敷地内に駐車場・駐輪場はございません。</p>
+                        </div>
                     </div>
 
                     <div className="about-texts-row">
@@ -122,42 +128,43 @@ function About() {
                     <p>芝浦工業大学学生自治会</p>
                     </div>
                 </div>
+                <img src={aboutImage} className="about-image" alt="exhibitionPoster"/>
+            </div>
             </div>
             
             <div className="access">
                 <h2>アクセス</h2>
+
+                <div className="accessContents">
+
+                    <div className="accessText">
                 
-                <ul className="accessContents">
-                    <li>
-                    <h3>芝浦工業大学 芝浦キャンパス</h3>
-                    <p>〒108-8548東京都港区芝浦3-9-14</p>
-                    </li>
+                        <h3>芝浦工業大学 芝浦キャンパス</h3>
+                        <p>〒108-8548東京都港区芝浦3-9-14</p>
 
-                    <ul className="list2">
-                        <li>
-                        <h4><a href="https://www.google.co.jp/maps/place/%E8%8A%9D%E6%B5%A6%E5%B7%A5%E6%A5%AD%E5%A4%A7%E5%AD%A6%E8%8A%9D%E6%B5%A6%E3%82%AD%E3%83%A3%E3%83%B3%E3%83%91%E3%82%B9/@35.6434231,139.7494067,19.35z/data=!4m13!1m7!3m6!1s0x60188bb4d9a8052b:0xc5aaaf0b3d68e69d!2z44CSMTA4LTAwMjMg5p2x5Lqs6YO95riv5Yy66Iqd5rWm77yT5LiB55uu77yZ4oiS77yR77yUIOiKnea1puW3pealreWkp-WtpiDoip3mtabjgq3jg6Pjg7Pjg5Hjgrk!3b1!8m2!3d35.6434443!4d139.7496359!3m4!1s0x60188bb4d90f4165:0xc16d0476cce240aa!8m2!3d35.6433728!4d139.7496946?hl=ja">大きな地図で見る</a></h4>
-                        </li>
-                        <li>
-                        <Icon>open_in_new</Icon>
-                        </li>
-                    </ul>
+                        <ul className="bigMap">
+                            <li>
+                                <h4><a href="https://www.google.co.jp/maps/place/%E8%8A%9D%E6%B5%A6%E5%B7%A5%E6%A5%AD%E5%A4%A7%E5%AD%A6%E8%8A%9D%E6%B5%A6%E3%82%AD%E3%83%A3%E3%83%B3%E3%83%91%E3%82%B9/@35.6434231,139.7494067,19.35z/data=!4m13!1m7!3m6!1s0x60188bb4d9a8052b:0xc5aaaf0b3d68e69d!2z44CSMTA4LTAwMjMg5p2x5Lqs6YO95riv5Yy66Iqd5rWm77yT5LiB55uu77yZ4oiS77yR77yUIOiKnea1puW3pealreWkp-WtpiDoip3mtabjgq3jg6Pjg7Pjg5Hjgrk!3b1!8m2!3d35.6434443!4d139.7496359!3m4!1s0x60188bb4d90f4165:0xc16d0476cce240aa!8m2!3d35.6433728!4d139.7496946?hl=ja">大きな地図で見る</a></h4>
+                            </li>
+                            <li>
+                                <Icon>open_in_new</Icon>
+                            </li>
+                        </ul>
 
-                    <li>
                         <h4>電車でのアクセス</h4>
                         <p>JR山手線 京浜東北線「田町駅」芝浦口から徒歩3分<br></br>
                         都営地下鉄 三田線・浅草線「三田駅」から徒歩5分</p>
-                    </li>
-                        
-                    <li>
+                    </div>
+
+                    <iframe className="accessMap" src="https://maps.google.co.jp/maps?output=embed&t=m&hl=ja&z=18&ll=35.6433503734224, 139.7496825387055&q=35.6433503734224, 139.7496825387055" frameborder="0" scrolling="no" width="auto"></iframe>
                     
-                    <iframe title="GoogleMap" className="map" src="https://maps.google.co.jp/maps?output=embed&t=m&hl=ja&z=18&ll=35.6433503734224, 139.7496825387055&q=35.6433503734224, 139.7496825387055" frameborder="0" scrolling="no" width="auto"></iframe>
-                    </li>
-                </ul>
+                </div>
             </div>
-        
         </div>
     </motion.div>
   );
 }
 
 export default About;
+
+
