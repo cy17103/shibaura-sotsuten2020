bf6be1205fa4c78febf689f46b52189d978f144e
NanaMejima <cy18273@shibaura-it.ac.jp>
2021-01-05 18:56:44 +0900

    Aboutのsp改行の位置変更

--- shibatest/src/About.js
+++ shibatest/src/About.js
@@ -28,14 +28,14 @@ function About() {
                 <br></br>
                 また、新型コロナウイルスの流行によって学生の大学生活・研究活動は例年とは大きく変化しました。<br></br>
                 その中で、デザイン工学部の学生がこれまで得てきた知識や経験を活かして取り組んだ研究や作品を展示しています。<br></br>
-                他にも在学中に取り組んだコンペやポートフォリオの展示も一部行っております。<br></br>
+                他にも在学中に取り組んだコンペやポート<br class="br-sp"></br>フォリオの展示も一部行っております。<br></br>
                 <br></br>
                 例年通り、大学での展示も行っております。<br></br>
                 ご興味がございましたら、ぜひお立ち寄りください。皆様のお越しを心よりお待ちしております。<br></br>
                 <br></br>
                     <div className="writername">
                      2020.3.10<br></br>
-                      芝浦工業大学デザイン工学部卒業・修了研究展委員
+                      芝浦工業大学デザイン工学部<br class="br-sp"></br>卒業・修了研究展委員
                     </div>
                 </p>
             </div>
@@ -70,7 +70,7 @@ function About() {
                 <div className="about-texts">
                     <div className="about-texts-row mg-16">
                     <h4>展覧会名</h4>
-                    <p>芝浦工業大学デザイン工学部卒業・修了研究展2021</p>
+                    <p>芝浦工業大学デザイン工学部<br class="br-sp"></br>卒業・修了研究展2021</p>
                     </div>
 
                     <div className="about-texts-row mg-16">
@@ -81,16 +81,16 @@ function About() {
                     <div className="about-texts-row mg-16">
                     <h4>会期</h4>
                         <div >
-                        <p>2021.3.14(日) - 3.28(日)<br></br>10:00 ~ 18:00 (最終入場時間 17:30)</p>
-                        <p className="about-subtext">*日曜日は休校日のため、ご入場できません。<br></br>*最終開催日は17:00までの入場となります。</p>
+                        <p>2021.3.14(日) - 3.28(日)<br></br>10:00 ~ 18:00 <br class="br-sp"></br>(最終入場時間 17:30)</p>
+                        <p className="about-subtext">*日曜日は休校日のため、ご入場<br class="br-sp"></br>できません。<br></br>*最終開催日は17:00までの入場<br class="br-sp"></br>となります。</p>
                         </div>
                     </div>
 
                     <div className="about-texts-row mg-16">
                         <h4>会場</h4>
                         <div>
-                         <p>芝浦工業大学芝浦キャンパス<br></br>〒108-8548 東京都港区芝浦3-9-14</p>
-                         <p className="about-subtext">*大学敷地内に駐車場・駐輪場はございません。</p>
+                         <p>芝浦工業大学芝浦キャンパス<br></br>〒108-8548<br class="br-sp"></br>東京都港区芝浦3-9-14</p>
+                         <p className="about-subtext">*大学敷地内に駐車場・駐輪場は<br class="br-sp"></br>ございません。</p>
                         </div>
                     </div>
 
@@ -125,8 +125,8 @@ function About() {
                         </a>
                         
                         <h4>電車でのアクセス</h4>
-                        <p>JR山手線 京浜東北線「田町駅」芝浦口から徒歩3分<br></br>
-                        都営地下鉄 三田線・浅草線「三田駅」から徒歩5分</p>
+                        <p>JR山手線 京浜東北線<br class="br-sp"></br>「田町駅」芝浦口から徒歩3分<br></br>
+                        都営地下鉄 三田線・浅草線<br class="br-sp"></br>「三田駅」から徒歩5分</p>
                     </div>
 
                     <iframe className="accessMap" src="https://maps.google.co.jp/maps?output=embed&t=m&hl=ja&z=18&ll=35.6433503734224, 139.7496825387055&q=35.6433503734224, 139.7496825387055" frameborder="0" scrolling="no" width="auto"></iframe>
